79f3f47403f6f1a5a156ac11be2530dc3e45e009
masumbillah360 <masumbillah6584@gmail.com>
2022-09-04 23:23:21 +0600

    update font size and bug fixed

--- app.js
+++ app.js
@@ -23,20 +23,23 @@ const viewQuran =(surahs)=>{
 const getSurah =async(surahNumber)=>{
     const res = await fetch(`https://api.alquran.cloud/v1/surah/${surahNumber}`);
     const surah = await res.json();
-    showFullSurah(surah.data.ayahs);
+    showFullSurah(surah.data);
+    console.log(surah.data);
 }
 
 const showFullSurah = (surah)=>{
     let ayatNum = 0;
-    document.getElementById('surah-name-modal').innerText = surah.name;
+
+    document.getElementById('surah-name-modal').innerText = `${surah.name} ${surah.englishName}`;
+    // modaltitle.class
     const ayahsContainer = document.getElementById('ayahs-container');
     ayahsContainer.textContent = "";
-    surah.forEach(ayahs=>{
+    surah.ayahs.forEach(ayahs=>{
         console.log(ayahs.text +" : "+ ayahs.number);
         const createAyasList = document.createElement('li');
         createAyasList.classList.add('text-end','row')
         createAyasList.innerHTML = `
-            <span class = "col-11">${ayahs.text}</span> <span class = "col-1">${ayatNum++}</span>
+            <span class = "col-11 fs-4">${ayahs.text}</span> <span class = "col-1 fs-4">${ayatNum++}</span>
         `
         ayahsContainer.appendChild(createAyasList);
         
